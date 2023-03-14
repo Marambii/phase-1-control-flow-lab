6df7a5990db21d2297b262dabded02b795695f45
Baruch Marambi Mwenda <marambib2019@gmail.com>
2023-03-14 15:25:47 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+
   // Write your code here!
+
+function scuberGreetingForInches(distance){
+  // Write your code here!
+  if (distance <= 400){
+    return "This is on me!";
+  }
+  else if (distance > 2000 && distance < 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (distance > 2500){
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC"? "ok, sounds good." :"No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "Not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."    
+  }
 }
